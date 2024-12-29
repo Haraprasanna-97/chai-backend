34bedb8b6236256c064d8e0ea24b3378db2dd806
Haraprasanna Misra <haraprasannamisra@gmail.com>
2024-12-29 20:16:11 +0530

    Added tweet functiionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,124 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+
+    if (!req.body.content) {
+        throw new ApiError(400, "content is required")
+    }
+
+    let tweet = {
+        owner: req.user._id,
+        content: req.body.content
+    }
+
+    const inserted = await Tweet.insertMany([tweet])
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, inserted, "Tweet fetched successfully")
+        )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+
+    const { userId } = req.params
+
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    let tweets = await Tweet.aggregate([
+        {
+            '$lookup': {
+                'from': 'users',
+                'localField': 'owner',
+                'foreignField': '_id',
+                'as': 'ownerDetails'
+            }
+        },
+        {
+            '$unwind': '$ownerDetails'
+        },
+        {
+            '$match': {
+                'owner': new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            "$addFields": {
+                "username": "$ownerDetails.username",
+                "avatar": "$ownerDetails.avatar"
+            }
+        },
+        {
+            '$project': {
+                'content': 1,
+                'username': 1,
+                'avatar': 1,
+                'createdAt': 1,
+                'updatedAt': 1,
+                '_id': 0
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { tweets }, "Fetched all tweets")
+        )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    let tweet = {
+        content: req.body.content
+    }
+
+    let tweetId = req.params.tweetId
+
+    let status = await Tweet.updateOne({
+        _id: new mongoose.Types.ObjectId(tweetId)
+    }, tweet)
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { status }, "Tweet updated")
+        )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    let tweetId = req.params.tweetId
+
+    let status = await Tweet.deleteOne({
+        _id: new mongoose.Types.ObjectId(tweetId)
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, { status }, "Tweet deleted")
+        )
 })
 
 export {
